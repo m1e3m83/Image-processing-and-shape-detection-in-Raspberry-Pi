8f99496a484dfa60c8c234ee06c8a266956e9045
mohamadmarandi <mohamadmarandi83@gmail.com>
2025-01-27 00:14:41 +0330

    developing simple area recognition for all elements in picutre

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
 import cv2 as cv
+#opencv
 
 cap = cv.VideoCapture(0)
+path = "image/image.png"
 scale = 1
 i = 0
-
-
-def remove_inner():
-    global contours, hierarchy
-
-    filtered_contours = []  # To store only the desired contours
-    for i, cnt in enumerate(contours):
-        # Check if the contour has a child (hierarchy[i][2] != -1) and that child has no siblings
-        if hierarchy[0][i][2] != -1 and hierarchy[0][hierarchy[0][i][2]][0] == -1:
-            continue  # Skip adding this contour to filtered_contours
-        filtered_contours.append(cnt)
-
-    # Update contours with filtered ones
-    contours = filtered_contours
+read_from_cam = True
 
 while True:
-    _, image = cap.read()
-
-    imgray = cv.cvtColor(cv.bitwise_not(image), cv.COLOR_BGR2GRAY)
-    ret, thresh = cv.threshold(imgray, 63, 255, 0)
+    if read_from_cam:
+        _, image = cap.read()
+    else:
+        image = cv.imread(path)
+    img = cv.bitwise_not(image)
+    imgBlur = cv.GaussianBlur(img, (7, 7), 1)
+    imgGray = cv.cvtColor(imgBlur, cv.COLOR_BGR2GRAY)
+    ret, thresh = cv.threshold(imgGray, 127, 255, 0)
+    #thresh = cv.adaptiveThreshold(imgGray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    contours = list(contours)
-    #remove_inner()
+    if len(contours) == 0:
+        continue
+    i %= len(contours)
 
-    drawimg = image.copy()
-    cv.drawContours(drawimg, contours, i, (0, 255, 0), 1)
+    drawing = image.copy()
+    try:
+        cv.drawContours(image, contours, i, (0, 255, 0), 5)
+    except:
+        pass
 
-    cv.imshow("shape number " + str(i), drawimg)
-    if(len(contours) != 0):
-        print(cv.contourArea(contours[i]) / scale * scale)
+    cv.imshow("shape", image)
+    print(cv.contourArea(contours[i]) / scale * scale)
 
     key = cv.waitKeyEx(1)
     if key == ord('q'):
         break
     elif key == ord('i'):
-        i = (i + 1) % len(contours)
+        i += 1
+    elif key == ord('r'):
+        i = 0
 
 
 cv.destroyAllWindows()

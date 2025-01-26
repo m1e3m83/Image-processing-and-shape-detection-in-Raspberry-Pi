c48afc8cbe0d341e6df6966a3ff424f552c03573
mohamadmarandi <mohamadmarandi83@gmail.com>
2025-01-26 17:32:44 +0330

    developing simple area recognition for all elements in picutre

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import cv2 as cv
-path = "image/images.png"
 
-image = cv.imread(path)
-cv.imshow("image", image)
+path = "image/image.png"
+i = 0
+
+img = cv.imread(path)
+image = cv.bitwise_not(img)
+
+imgray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+ret, thresh = cv.threshold(imgray, 127, 255, 0)
+contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+cv.drawContours(img, contours, 0, (0, 255, 0), 1)
+
+area = []
+for contour in contours:
+    area.append(cv.contourArea(contour))
+
+print(hierarchy)
+print(area)
+
+cv.imshow("image", img)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
